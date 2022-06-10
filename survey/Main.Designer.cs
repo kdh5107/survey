6a83ad3383ebe9e2de71ef97ebfae4bd759cdb97
kdh5107 <bjmb12@naver.com>
2022-06-11 02:39:59 +0900

    fix_1

---
+++ survey/Main.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace survey
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.참여자 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 12F);
+            this.label1.Location = new System.Drawing.Point(192, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Survey Master";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "관리자용";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // 참여자
+            // 
+            this.참여자.Location = new System.Drawing.Point(318, 290);
+            this.참여자.Name = "참여자";
+            this.참여자.Size = new System.Drawing.Size(120, 50);
+            this.참여자.TabIndex = 2;
+            this.참여자.Text = "참여자용";
+            this.참여자.UseVisualStyleBackColor = true;
+            this.참여자.Click += new System.EventHandler(this.참여자용_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(520, 450);
+            this.Controls.Add(this.참여자);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button 참여자;
+    }
+}
+
--- survey/Program.cs
+++ survey/Program.cs
@@ -16,7 +16,7 @@ namespace survey
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
